6c148cf1385bd45f418ddf33bb48ea52b6e2c4e0
34Mohdirshad <mohdirshadmi36542@gmail.com>
2026-03-25 00:02:55 +0530

    fix(db): add explicit dbName for Atlas compatibility and finalize routing

--- server/config/db.js
+++ server/config/db.js
@@ -13,6 +13,7 @@ const connectDB = async () => {
     try {
         const db = await mongoose.connect(process.env.MONGODB_URI, {
             serverSelectionTimeoutMS: 5000,
+            dbName: 'medical_gallery'
         });
         isConnected = !!db.connections[0].readyState;
         console.log('MongoDB connected');
